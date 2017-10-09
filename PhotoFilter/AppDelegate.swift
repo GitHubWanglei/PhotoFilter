3a5f1244f39b54336d207d866f8a2c3b2e660003
GitHubWanglei <963256255@qq.com>
2017-10-09 15:09:00 +0800

    .
    .

--- PhotoFilter/AppDelegate.swift
+++ PhotoFilter/AppDelegate.swift
@@ -25,11 +25,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
     func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-        if let mainWindow = NSApp.window(withWindowNumber: windowNumber) {
-            mainWindow.makeKeyAndOrderFront(self)
-            return true
+        if flag == false {
+            if let mainWindow = NSApp.window(withWindowNumber: windowNumber) {
+                mainWindow.makeKeyAndOrderFront(self)
+                return true
+            }
         }
         return flag
     }
